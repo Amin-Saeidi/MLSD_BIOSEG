7ab64875892bdd136053ae40c96723011c545beb
Amin-Saeidi <109428488+Amin-Saeidi@users.noreply.github.com>
2023-08-09 19:36:24 +0330

    Update test.py

--- test/test.py
+++ test/test.py
@@ -1,16 +1,23 @@
 import sys
-import PIL as Image
-import io
-
 sys.path.append("..")
 
 import application.Classifier as classifier
 
-
 if __name__ == "__main__":
+    
     with open("sample.png", "rb") as image:
         f = image.read()
         b = bytearray(f)
+    
     classifier_model = classifier.classifier()
     image_class, image_class_name = classifier.preprocessing_and_predict(classifier_model, b)
-    print(image_class_name)
+    
+    if image_class_name == "CELL MEMBRANE":
+        test_checker = True
+    else:
+        test_checker = False
+
+    if test_checker == False:
+        raise Exception("Sorry, Model is not performing well! - please check the classifier model")
+    else:
+        print("MODEL IS ALRIGHT! - ALL THE TESTS PASSED :)")
