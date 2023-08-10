38740abd9387a43d7605b6f03ffebd9b163c807a
Amin-Saeidi <109428488+Amin-Saeidi@users.noreply.github.com>
2023-08-10 13:44:09 +0330

    Update Classifier.py

--- application/Classifier.py
+++ application/Classifier.py
@@ -3,39 +3,19 @@ from torchvision import models, transforms
 import torch
 import io
 import requests
-import os
-import sys
-import logging
 
 
-
-
-def classifier(type = "app"):
+def classifier():
     # Define the ResNet model with the same architecture as your finetuned model
     model = models.resnet50(num_classes=2) # Replace 2 with the number of classes in your finetuned model
+    
     url = 'https://drive.usercontent.google.com/download?id=19pOxhX9t-1fbJuAF8EHMEnEcdvM2ruAL&export=download&authuser=0&confirm=t&uuid=944c801c-735f-4c8a-9342-48eb8c426395&at=AC2mKKTGKC3HpcBooV6yXcunzL88:1691560364241'
     r = requests.get(url, allow_redirects=True)
-            
-    if type == "app":
-        sys.path.append("..")
-        if not os.path.isfile("./var/lib/data/resnet_ft.pt"):
-            open('./var/lib/data/resnet_ft', 'wb').write(r.content)
-            print("RES-net Model Added to Disk")
-            logging.info('RES-net Model Added to Disk')
-            
-        # Load the saved weights into the model
-        checkpoint = torch.load('./var/lib/data/resnet_ft.pt', map_location=torch.device('cpu'))
-    else:
-        print("GGG")
-        if not os.path.isfile("resnet_ft.pt"):
-            print("GOG")
-            open('resnet_ft', 'wb').write(r.content)
-            print("RES-net Model Added to test file")
-            logging.info('RES-net Model Added to test file')
-    
-        # Load the saved weights into the model
-        checkpoint = torch.load('./resnet_ft.pt', map_location=torch.device('cpu'))
-    
+
+    open('resnet_ft.pt', 'wb').write(r.content)
+
+    # Load the saved weights into the model
+    checkpoint = torch.load('resnet_ft.pt', map_location=torch.device('cpu'))
     # print(checkpoint.keys())  # Print the keys of the checkpoint dictionary
     model.load_state_dict(checkpoint)  # Replace with the actual key name in your checkpoint dictionary
     # print(model)
