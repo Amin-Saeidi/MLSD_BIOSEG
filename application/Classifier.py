d78be1937379375cb09587dc3d64b89936ea0b1b
Amin-Saeidi <109428488+Amin-Saeidi@users.noreply.github.com>
2023-08-10 12:46:50 +0330

    Update Classifier.py

--- application/Classifier.py
+++ application/Classifier.py
@@ -8,15 +8,19 @@ import requests
 def classifier():
     # Define the ResNet model with the same architecture as your finetuned model
     model = models.resnet50(num_classes=2) # Replace 2 with the number of classes in your finetuned model
-    
-    url = 'https://drive.usercontent.google.com/download?id=19pOxhX9t-1fbJuAF8EHMEnEcdvM2ruAL&export=download&authuser=0&confirm=t&uuid=944c801c-735f-4c8a-9342-48eb8c426395&at=AC2mKKTGKC3HpcBooV6yXcunzL88:1691560364241'
-    r = requests.get(url, allow_redirects=True)
 
-    open('resnet_ft.pt', 'wb').write(r.content)
+
+    if not os.path.isfile("./var/lib/data/resnet_ft.pt"):
+
+        url = 'https://drive.usercontent.google.com/download?id=19pOxhX9t-1fbJuAF8EHMEnEcdvM2ruAL&export=download&authuser=0&confirm=t&uuid=944c801c-735f-4c8a-9342-48eb8c426395&at=AC2mKKTGKC3HpcBooV6yXcunzL88:1691560364241'
+        r = requests.get(url, allow_redirects=True)
+        open('./var/lib/data/resnet_ft', 'wb').write(r.content)
+        print("RES-net Model Added to Disk")
+        logging.info('RES-net Model Added to Disk')
 
 
     # Load the saved weights into the model
-    checkpoint = torch.load('resnet_ft.pt', map_location=torch.device('cpu'))
+    checkpoint = torch.load('./var/lib/data/resnet_ft.pt', map_location=torch.device('cpu'))
     # print(checkpoint.keys())  # Print the keys of the checkpoint dictionary
     model.load_state_dict(checkpoint)  # Replace with the actual key name in your checkpoint dictionary
     # print(model)
@@ -38,7 +42,7 @@ def preprocessing_and_predict(model, image_bytes):
     output = model(tensor)
 
     _, predicted = torch.max(output.data, 1)
-    print(predicted)
+    # print(predicted)
     if predicted[0] == 1:
         class_name = "CELL MEMBRANE"
     else:
